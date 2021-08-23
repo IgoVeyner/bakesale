71c7e6041e4e5d85be44f5c6db43196750e5f5b1
Igor Veyner <igorveyner95@gmail.com>
2021-08-23 18:05:36 -0400

    create and use api to get list of deals

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react'
+import ajax from './src/ajax'
+import DealList from './src/components/DealList'
 
 export default function App() {
+  const [deals, setDeals] = useState([])
+
+  useEffect(() => {
+    ajax.fetchInitialDeals().then(fetchedDeals => {
+      setDeals(fetchedDeals)
+    })
+  }, [])
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Bakesale!</Text>
-      <StatusBar style="auto" />
+      {
+        deals.length > 0 ? (
+          <DealList deals={deals} />
+        ) : (
+          <Text style={styles.header}>Bakesale!</Text>
+        )
+      }
     </View>
   );
 }
---
+++ src/components/DealList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const DealList = (deals) => {
+  return (
+    <View>
+      <Text>
+        Deals...
+      </Text>
+    </View>
+  )
+}
+
+export default DealList
