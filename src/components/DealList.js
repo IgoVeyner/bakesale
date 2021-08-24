236a104517fce98437a7d8fe1c1f274cabc6ce6a
Igor Veyner <igorveyner95@gmail.com>
2021-08-24 13:24:10 -0400

    add flatlist and render list of deals

--- src/components/DealList.js
+++ src/components/DealList.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import PropTypes from 'prop-types'
 
-const DealList = (deals) => {
+const DealList = ({ deals }) => {
   return (
-    <View>
-      <Text>
-        Deals...
-      </Text>
+    <View style={styles.list}>
+      <FlatList 
+        data={deals}
+        renderItem={({item}) => <Text>{item.title}</Text>}
+      />
     </View>
   )
+}
+
+const styles = StyleSheet.create({
+  list: {
+    backgroundColor: '#eee',
+    flex: 1,
+    width: '100%',
+    paddingTop: 50
+  }
+})
+
+DealList.prototypes = {
+  deals: PropTypes.array.isRequired
 }
 
 export default DealList
