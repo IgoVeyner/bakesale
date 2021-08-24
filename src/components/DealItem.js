7ff868aab5df9fa555a0ec132f2eb087c1e70901
Igor Veyner <igorveyner95@gmail.com>
2021-08-24 13:40:57 -0400

    add styling

--- src/components/DealItem.js
+++ src/components/DealItem.js
@@ -6,24 +6,59 @@ import { priceDisplay } from '../util'
 
 const DealItem = ({ deal }) => {
   return (
-    <View>
+    <View style={styles.deal}>
       <Image 
         source={{ uri: deal.media[0] }} 
         style={styles.image}
       />
-      <View>
-        <Text>{deal.title}</Text>
-        <Text>{priceDisplay(deal.price)}</Text>
-        <Text>{deal.cause.name}</Text>
+      <View style={styles.info}>
+        <Text style={styles.title}>{deal.title}</Text>
+        <View style={styles.footer}>
+          <Text style={styles.cause}>{deal.cause.name}</Text>
+          <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+        </View>
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  deal: {
+    marginHorizontal: 12,
+    marginTop: 12,
+  },
+  
   image: {
     width: '100%',
-    height: 150
+    height: 150,
+    backgroundColor: '#ccc'
+  },
+
+  info: {
+    padding: 10,
+    backgroundColor: '#fff',
+    borderColor: '#bbb',
+    borderWidth: '1',
+    borderTopWidth: '0',
+  },
+
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+
+  footer: {
+    flexDirection: 'row',
+  },
+
+  cause: {
+    flex: 2,
+  },
+
+  price: {
+    flex: 1,
+    textAlign: 'right',
   },
 })
 
